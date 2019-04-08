8ad2fe749a156e4849641c90aca6c6dc233a3a9d
Robert Gańko <rganko@speednet.pl>
2019-04-08 10:30:30 +0200

    keep custom stuff together

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import isEmpty from 'lodash/isEmpty';
 import isFunction from 'lodash/isFunction';
 
 import createRootReducer, { getInitialState } from './reducers';
@@ -36,11 +37,43 @@ const connect = (mapStateToProps, mapDispatchToProps) => WrappedComponent => {
 
 const configureStore = (preLoadedState = {}) => {
   // combine preLoadedState and reducers
-
   const reducer = createRootReducer();
   const initialState = getInitialState(reducer);
 
   return { initialState: { ...initialState, ...preLoadedState }, reducer };
+};
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const stateLogger = (action, newState) => {
+  try {
+    if (isEmpty(action)) {
+      console.info("%cInit State: ", "color: green", newState);
+      return;
+    }
+    console.info("%caction: ", "color: red", action);
+    console.info("%cnewState: ", "color: green", newState);
+    console.info('')
+  } catch(e) {}
+};
+
+const combineReducers = (reducers) => {
+  return (state = {}, action) => {
+    const newState = Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](
+          state[key],
+          action
+        );
+        return nextState
+      },
+      {});
+    if (isDevelopment) {
+      stateLogger(action, newState);
+    }
+
+    return newState;
+  }
 };
 
 export default function Provider({ store, children }){
@@ -54,4 +87,4 @@ export default function Provider({ store, children }){
   )
 };
 
-export { connect, configureStore }
+export { connect, configureStore, combineReducers }
